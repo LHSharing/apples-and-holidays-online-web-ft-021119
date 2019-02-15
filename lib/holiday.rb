d6139d41bfb9937c4f96799505d80bd4d8861009
Lu Daley <daleyluis@gmail.com>
2019-02-15 19:39:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
 #key = :christmas
 #value = ["Lights", "Wreath"]
 #("  Christmas: Lights, Wreath")
-holiday_hash.each {|key, value| key.capitalize!}
+holiday_hash.each {|key, value| key.capitalize}
 
 binding.pry
 
